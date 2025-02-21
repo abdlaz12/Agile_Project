bc9a693b77819411b828110eaf96fd380109f546
ajish <azisgodgod@gmail.com>
2025-02-21 09:41:27 +0700

    Pembuatan project and template frontend

---
+++ building_web/core/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'core/index.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+def about(request):
+    return render(request, 'core/about.html')
+
